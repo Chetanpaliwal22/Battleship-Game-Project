d796ca449de46cef3c1385b90dd2e618f4dfd091
HongyuShen <hongyushen9914@gmail.com>
2019-07-12 14:41:23 -0400

    New merge

--- src/model/Ship.java
+++ src/model/Ship.java
@@ -1,5 +1,6 @@
 package model;
 
+import constants.Constants;
 import tools.Coordinate;
 import view.MainWindow;
 
@@ -9,7 +10,7 @@ import java.util.List;
 public class Ship {
 
     private ArrayList<Coordinate> position = new ArrayList<Coordinate>();
-    private ArrayList<Coordinate> hitPosition  = new ArrayList<Coordinate>();
+    private ArrayList<Coordinate> hitPosition = new ArrayList<Coordinate>();
     private int size;
     private boolean isAlive = true;
 
@@ -19,63 +20,13 @@ public class Ship {
      * @param size
      * @param position
      */
-    /*public Ship(int size, Coordinate[] position) {
+    public Ship(int size, Coordinate[] position) {
 
-        for(int i=0; i<position.length; i++){
+        for (int i = 0; i < position.length; i++) {
             this.position.add(position[i]);
         }
         this.size = size;
-    }*/
 
-    /**
-     * argument constructor
-     * @param shipSize
-     * @param shipDirection
-     * @param shipPivotGridX
-     * @param shipPivotGridY
-     */
-    public Ship(int shipSize, int shipDirection, int shipPivotGridX, int shipPivotGridY) {
-
-        this.size = shipSize;
-
-        this.position = orientationToPosition(shipSize, shipDirection, shipPivotGridX, shipPivotGridY);
-
-        recalculate(shipDirection, shipPivotGridX, shipPivotGridY);
-    }
-
-
-    /**
-     * convert orientation to position
-     * @param shipSize
-     * @param shipDirection
-     * @param shipPivotGridX
-     * @param shipPivotGridY
-     * @return
-     */
-    private ArrayList<Coordinate> orientationToPosition(int shipSize, int shipDirection, int shipPivotGridX, int shipPivotGridY){
-
-        ArrayList<Coordinate> newPosition = new ArrayList<Coordinate>();
-        Coordinate increment = new Coordinate(0, 0);
-
-        if( shipDirection == 1 ){
-            increment.x = 1;
-
-        } else if( shipDirection == 2 ){
-            increment.y = -1;
-
-        } else if( shipDirection == 3 ){
-            increment.x = -1;
-
-        } else if( shipDirection == 4 ){
-            increment.y = 1;
-        }
-
-        Coordinate tmp = new Coordinate(0,0);
-        for(int i=0; i<shipSize; i++){
-            newPosition.add( new Coordinate( shipPivotGridX+(i*increment.y), shipPivotGridY+(i*increment.x) ) );
-        }
-
-        return newPosition;
     }
 
     /**
@@ -83,7 +34,9 @@ public class Ship {
      *
      * @return
      */
-    public ArrayList<Coordinate> getPosition() { return position; }
+    public ArrayList<Coordinate> getPosition() {
+        return position;
+    }
 
     /**
      * get size of the ship
@@ -120,7 +73,7 @@ public class Ship {
 
         if (isAlive) {
 
-            for (int i = 0; i < this.position.size(); i++){
+            for (int i = 0; i < this.position.size(); i++) {
 
                 if (this.position.get(i).equals(target)) {
 
@@ -162,12 +115,6 @@ public class Ship {
     }
 
 
-
-
-
-    ////////////////////////////////////////////////////////////
-
-
     // The coordinates based on the window
     public double x = 0, y = 0;
 
@@ -184,6 +131,12 @@ public class Ship {
 
     public boolean validity = true;
 
+    // Constructor for initializing the attributes
+    public Ship(int shipLength, int shipDirection, int shipPivotGridX, int shipPivotGridY) {
+        length = shipLength;
+
+        recalculate(shipDirection, shipPivotGridX, shipPivotGridY);
+    }
 
     // Every time the direction or location of the ship changed, recalculate its
     // occupied grids
@@ -198,7 +151,7 @@ public class Ship {
         occupiedGridX = new ArrayList<Integer>();
         occupiedGridY = new ArrayList<Integer>();
 
-        if (this.size == 2) {
+        if (length == 2) {
             // Check the ship direction
             if (direction == 1) {
                 occupiedGridX.add(shipPivotGridX);
@@ -225,7 +178,7 @@ public class Ship {
                 occupiedGridX.add(shipPivotGridX);
                 occupiedGridY.add(shipPivotGridY + 1);
             }
-        } else if (size == 3) {
+        } else if (length == 3) {
             // Check the ship direction
             if (direction == 1 | direction == 3) {
                 occupiedGridX.add(shipPivotGridX);
@@ -246,7 +199,7 @@ public class Ship {
                 occupiedGridX.add(shipPivotGridX);
                 occupiedGridY.add(shipPivotGridY + 1);
             }
-        } else if (size == 4) {
+        } else if (length == 4) {
             // Check the ship direction
             if (direction == 1) {
                 occupiedGridX.add(shipPivotGridX);
@@ -297,7 +250,7 @@ public class Ship {
                 occupiedGridX.add(shipPivotGridX);
                 occupiedGridY.add(shipPivotGridY + 2);
             }
-        } else if (size == 5) {
+        } else if (length == 5) {
             // Check the ship direction
             if (direction == 1 | direction == 3) {
                 occupiedGridX.add(shipPivotGridX);
@@ -336,18 +289,16 @@ public class Ship {
     // Check if the ship is outside the board
     public boolean validateLocation() {
         for (int i = 0; i < occupiedGridX.size(); i++) {
-            if (occupiedGridX.get(i) < 0 | occupiedGridX.get(i) > 10) {
+            if (occupiedGridX.get(i) < 0 | occupiedGridX.get(i) >= Constants.BOARD_SIZE.y) {
                 validity = false;
                 return false;
-
             }
         }
 
         for (int i = 0; i < occupiedGridY.size(); i++) {
-            if (occupiedGridY.get(i) < 0 | occupiedGridY.get(i) > 8) {
+            if (occupiedGridY.get(i) < 0 | occupiedGridY.get(i) >= Constants.BOARD_SIZE.x) {
                 validity = false;
                 return false;
-
             }
         }
 

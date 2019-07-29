73e293dd39849fc206cb5c46b4ac41b05e1440c7
Chetanpaliwal22 <Chetanpaliwal22@gmail.com>
2019-07-29 12:33:07 -0400

    pushing older version

--- src/main/Game.java
+++ src/main/Game.java
@@ -7,23 +7,27 @@ import jaco.mp3.player.MP3Player;
 import view.MainWindow;
 
 /**
- * This class is the starting point of the Game. 
+ * This class is the starting point of the Game.
  */
 
 public class Game {
 
-    public static MainWindow mainWindow;
+	public static MainWindow mainWindow;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // Start the game //
+		// Start the game //
 
-        mainWindow = new MainWindow();
-        mainWindow.setVisible(true);
-       
-        //try { new MP3Player(new File(Constants.MUSIC_FILE_NAME)).play(); }
+		mainWindow = new MainWindow();
+		mainWindow.setVisible(true);
 
-        //catch(Exception e) { System.out.println("Could not find the MP3Player class."); }
+		try {
+			new MP3Player(new File(Constants.MUSIC_FILE_NAME)).play();
+		}
 
-    }
+		catch (Exception e) {
+			System.out.println("Could not find the MP3Player class.");
+		}
+
+	}
 }

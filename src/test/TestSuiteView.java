9c9425ca69e8934d0f2bd54f68652600f256c65b
HongyuShen <hongyushen9914@gmail.com>
2019-08-02 17:59:07 -0400

    View test class

---
+++ src/test/TestSuiteView.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Test Suite class to run all the test from model package
+ */
+@RunWith(Suite.class)
+@SuiteClasses({RendererTest.class
+})
+
+public class TestSuiteView {
+
+}
